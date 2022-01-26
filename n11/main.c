6125004e9025a936c189164b43b5dfdac86f6b44
Alexander Kuznetsov <kuznetsovup@yandex.ru>
2022-01-26 09:23:35 +0300

    “lab11”

---
+++ n11/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <struct.h>
+
+int main() {
+    struct square figure;
+    
+    float a;
+    scanf("%f", &a);
+    float A_x = a;
+    scanf("%f", &a);
+    float B_x = a;
+    scanf("%f", &a);
+    float C_x = a;
+    scanf("%f", &a);
+    float D_x = a;
+    scanf("%f", &a);
+    float A_y = a;
+    scanf("%f", &a);
+    float B_y = a;
+    scanf("%f", &a);
+    float C_y = a;
+    scanf("%f", &a);
+    float D_y = a;
+    
+    figure = structure(A_x, V)
+}
+
